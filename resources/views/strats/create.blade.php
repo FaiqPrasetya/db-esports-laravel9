819732b675be5ebca27f8f7beb3516d16b55dedb
AndreMaulanaMustofa <andremm73@gmail.com>
2022-05-25 12:04:38 +0700

    Required Added on Create Form

--- resources/views/strats/create.blade.php
+++ resources/views/strats/create.blade.php
@@ -24,7 +24,7 @@
                         {{-- map name --}}
                         <div class="form-group">
                             <label for="map_name">Map</label>
-                            <select name="map_name" id="map_name" class="form-control">
+                            <select name="map_name" id="map_name" class="form-control" required>
                                 <option value="">Select Map</option>
                                 @foreach($maps as $map)
                                     <option value="{{ $map->map_name }}">{{ $map->map_name }}</option>
@@ -36,7 +36,7 @@
                         <div class="form-group">
                             <label for="strats_image">File</label>
                             <input type="file" class="file-control @error('strats_image') is-invalid @enderror"
-                                id="strats_image" name="strats_image" placeholder="Upload File" value="{{ old('strats_image')}}">
+                                id="strats_image" name="strats_image" required placeholder="Upload File" value="{{ old('strats_image')}}">
                             @error('strats_image')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
@@ -47,7 +47,7 @@
                         <div class="form-group">
                             <label for="strats_name">Strats Name</label>
                             <input type="text" class="form-control @error('strats_name') is-invalid @enderror"
-                                id="strats_name" name="strats_name" placeholder="Strats Name" value="{{ old('strats_name')}}">
+                                id="strats_name" name="strats_name" required placeholder="Strats Name" value="{{ old('strats_name')}}">
                             @error('strats_name')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
@@ -59,7 +59,7 @@
                             <label for="strats_desc">Strats Description</label>
                             <textarea name="strats_desc" id="strats_desc" cols="30" rows="10"
                                 class="form-control ckeditor @error('strats_desc') is-invalid @enderror"
-                                placeholder="Strats Description">{{ old('strats_desc')}}</textarea>
+                                placeholder="Strats Description" required>{{ old('strats_desc')}}</textarea>
                             @error('strats_desc')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
@@ -69,7 +69,7 @@
                         {{-- Agent one selection --}}
                         <div class="form-group">
                             <label for="agent_one">Agent One</label>
-                            <select name="agent_one" id="agent_one" class="form-control">
+                            <select name="agent_one" id="agent_one" class="form-control" required>
                                 <option value="">Select Agent</option>
                                 @foreach($agents as $agent)
                                     <option value="{{ $agent->agent_name }}">{{ $agent->agent_name }}</option>
@@ -82,7 +82,7 @@
                             <label for="agent_one_desc">Agent One Description</label>
                             <textarea name="agent_one_desc" id="agent_one_desc" cols="30" rows="10"
                                 class="form-control ckeditor @error('agent_one_desc') is-invalid @enderror"
-                                placeholder="Agent One Description">{{ old('agent_one_desc')}}</textarea>
+                                placeholder="Agent One Description" required>{{ old('agent_one_desc')}}</textarea>
                             @error('agent_one_desc')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
@@ -92,7 +92,7 @@
                         {{-- Agent two selection --}}
                         <div class="form-group">
                             <label for="agent_two">Agent Two</label>
-                            <select name="agent_two" id="agent_two" class="form-control">
+                            <select name="agent_two" id="agent_two" class="form-control" required>
                                 <option value="">Select Agent</option>
                                 @foreach($agents as $agent)
                                     <option value="{{ $agent->agent_name }}">{{ $agent->agent_name }}</option>
@@ -105,7 +105,7 @@
                             <label for="agent_two_desc">Agent Two Description</label>
                             <textarea name="agent_two_desc" id="agent_two_desc" cols="30" rows="10"
                                 class="form-control ckeditor @error('agent_two_desc') is-invalid @enderror"
-                                placeholder="Agent Two Description">{{ old('agent_two_desc')}}</textarea>
+                                placeholder="Agent Two Description" required>{{ old('agent_two_desc')}}</textarea>
                             @error('agent_two_desc')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
@@ -115,7 +115,7 @@
                         {{-- Agent three selection --}}
                         <div class="form-group">
                             <label for="agent_three">Agent Three</label>
-                            <select name="agent_three" id="agent_three" class="form-control">
+                            <select name="agent_three" id="agent_three" class="form-control" required>
                                 <option value="">Select Agent</option>
                                 @foreach($agents as $agent)
                                     <option value="{{ $agent->agent_name }}">{{ $agent->agent_name }}</option>
@@ -128,7 +128,7 @@
                             <label for="agent_three_desc">Agent Three Description</label>
                             <textarea name="agent_three_desc" id="agent_three_desc" cols="30" rows="10"
                                 class="form-control ckeditor @error('agent_three_desc') is-invalid @enderror"
-                                placeholder="Agent Three Description">{{ old('agent_three_desc')}}</textarea>
+                                placeholder="Agent Three Description" required>{{ old('agent_three_desc')}}</textarea>
                             @error('agent_three_desc')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
@@ -138,7 +138,7 @@
                         {{-- Agent four selection --}}
                         <div class="form-group">
                             <label for="agent_four">Agent Four</label>
-                            <select name="agent_four" id="agent_four" class="form-control">
+                            <select name="agent_four" id="agent_four" class="form-control" required>
                                 <option value="">Select Agent</option>
                                 @foreach($agents as $agent)
                                     <option value="{{ $agent->agent_name }}">{{ $agent->agent_name }}</option>
@@ -151,7 +151,7 @@
                             <label for="agent_four_desc">Agent Four Description</label>
                             <textarea name="agent_four_desc" id="agent_four_desc" cols="30" rows="10"
                                 class="form-control ckeditor @error('agent_four_desc') is-invalid @enderror"
-                                placeholder="Agent Four Description">{{ old('agent_four_desc')}}</textarea>
+                                placeholder="Agent Four Description" required>{{ old('agent_four_desc')}}</textarea>
                             @error('agent_four_desc')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
@@ -161,7 +161,7 @@
                         {{-- Agent five selection --}}
                         <div class="form-group">
                             <label for="agent_five">Agent Five</label>
-                            <select name="agent_five" id="agent_five" class="form-control">
+                            <select name="agent_five" id="agent_five" class="form-control" required>
                                 <option value="">Select Agent</option>
                                 @foreach($agents as $agent)
                                     <option value="{{ $agent->agent_name }}">{{ $agent->agent_name }}</option>
@@ -174,7 +174,7 @@
                             <label for="agent_five_desc">Agent Five Description</label>
                             <textarea name="agent_five_desc" id="agent_five_desc" cols="30" rows="10"
                                 class="form-control ckeditor @error('agent_five_desc') is-invalid @enderror"
-                                placeholder="Agent Five Description">{{ old('agent_five_desc')}}</textarea>
+                                placeholder="Agent Five Description" required>{{ old('agent_five_desc')}}</textarea>
                             @error('agent_five_desc')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
