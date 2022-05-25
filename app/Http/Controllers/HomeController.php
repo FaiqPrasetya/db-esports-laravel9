4ada0f152dcf1cf55bbe0034da7207b354948370
FaiqPrasetya <2041720239@student.polinema.ac.id>
2022-05-25 20:08:32 +0700

    HomeController added

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Maps;
+
+class HomeController extends Controller
+{
+    //
+    public function index() {
+        $maps = Maps::all();
+
+        return view('menu.main-menu.home', compact('maps'));
+    }
+}
