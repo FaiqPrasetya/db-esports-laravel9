0c731456aab31b1fdd4a835fdb726587750a5b87
AndreMaulanaMustofa <andremm73@gmail.com>
2022-06-03 14:05:10 +0700

    Function View Added

--- app/Http/Controllers/MapsController.php
+++ app/Http/Controllers/MapsController.php
@@ -15,32 +15,37 @@ class MapsController extends Controller
     }
 
     public function haven(){
-        $strats = DB::table('strats')->where('map_name', 'haven')->paginate(10);
+        $strats = DB::table('strats')->where('map_name', 'Haven')->paginate(10);
         return view('maps.Haven', compact('strats'));
     }
 
     public function icebox(){
-        $strats = DB::table('strats')->where('map_name', 'icebox')->paginate(10);
+        $strats = DB::table('strats')->where('map_name', 'Icebox')->paginate(10);
         return view('maps.Icebox', compact('strats'));
     }
 
     public function split(){
-        $strats = DB::table('strats')->where('map_name', 'split')->paginate(10);
+        $strats = DB::table('strats')->where('map_name', 'Split')->paginate(10);
         return view('maps.Split', compact('strats'));
     }
 
     public function breeze(){
-        $strats = DB::table('strats')->where('map_name', 'breeze')->paginate(10);
+        $strats = DB::table('strats')->where('map_name', 'Breeze')->paginate(10);
         return view('maps.Breeze', compact('strats'));
     }
 
     public function bind(){
-        $strats = DB::table('strats')->where('map_name', 'bind')->paginate(10);
+        $strats = DB::table('strats')->where('map_name', 'Bind')->paginate(10);
         return view('maps.Bind', compact('strats'));
     }
 
     public function ascent(){
-        $strats = DB::table('strats')->where('map_name', 'ascent')->paginate(10);
+        $strats = DB::table('strats')->where('map_name', 'Ascent')->paginate(10);
         return view('maps.Ascent', compact('strats'));
+    }
+
+    public function view($id){
+        $strats = Strats::find($id);
+        return view('maps.View', compact('strats'));
     }
 }
